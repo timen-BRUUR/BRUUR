895f317a4aa7bd2dfb0fb674d084d4ad262287bf
TimenVDH <timen.vanderhaeghen@gmail.com>
2024-07-17 21:42:33 +0200

    commit

--- bruur.js
+++ bruur.js
@@ -1,3 +1,4 @@
+// TEST
 <script src="https://unpkg.com/split-type"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"></script>
